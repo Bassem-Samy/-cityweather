89b73e911a8197668593a8d056c11699004fd8e9
Bassem Wissa <bassem.samy@hotmail.com>
2023-01-07 15:27:20 +0100

    extract files, disable wild card imports

---
+++ app/src/main/kotlin/com/accu/cityweather/location/CityFromLocationProvider.kt
@@ -0,0 +1,37 @@
+package com.accu.cityweather.location
+
+import android.app.Application
+import android.location.Address
+import android.location.Geocoder
+import android.os.Build
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.Locale
+import kotlin.coroutines.resume
+
+interface CityFromLocationProvider {
+    suspend fun getCityName(
+        longitude: Double,
+        latitude: Double,
+        locale: Locale = Locale.ENGLISH
+    ): String?
+}
+
+class GeoCoderCityFromLocationProvider(private val application: Application) :
+    CityFromLocationProvider {
+    override suspend fun getCityName(longitude: Double, latitude: Double, locale: Locale): String? {
+        return suspendCancellableCoroutine { continuation ->
+            val geoCoder = Geocoder(application, locale)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                geoCoder.getFromLocation(latitude, longitude, 1) {
+                    continuation.resume(it.getFirstCity())
+                }
+            } else {
+                @Suppress("DEPRECATION")
+                val addresses = geoCoder.getFromLocation(latitude, longitude, 1)
+                continuation.resume(addresses.getFirstCity())
+            }
+        }
+    }
+
+    private fun List<Address>?.getFirstCity(): String? = this?.firstOrNull()?.locality
+}
